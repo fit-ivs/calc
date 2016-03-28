2a5bbb38b14a8f202eaf322d28255df307d372be
Jakub Zárybnický <kuba.zarybnicky@post.cz>
2016-03-28 19:20:26 +0200

    Add parser - first draft, untested

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,7 +3,65 @@ module Lib
   (
     -- * Exported functions
     inc
+  , parse
+  , evaluate
+  , Expr()
   ) where
+
+import Control.Monad (void)
+import Control.Applicative (empty)
+import Text.Megaparsec (runParser, ParseError, (<|>), spaceChar, between)
+import Text.Megaparsec.Expr (Operator(Prefix, InfixL), makeExprParser)
+import Text.Megaparsec.String (Parser)
+import qualified Text.Megaparsec.Lexer as L
+
+data Expr a =
+      Number a
+    | Hole
+    | Add (Expr a) (Expr a)
+    | Mul (Expr a) (Expr a)
+    | Sub (Expr a) (Expr a)
+    | Div (Expr a) (Expr a)
+    | Mod (Expr a) (Expr a)
+    | Negate (Expr a)
+    deriving (Show, Eq)
+
+sc :: Parser ()
+sc = L.space (void spaceChar) empty empty
+
+symbol :: String -> Parser String
+symbol = L.symbol sc
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+operators :: Num a => [[Operator Parser (Expr a)]]
+operators =
+  [ [ Prefix (symbol "-" *> pure Negate) ]
+  , [ InfixL (symbol "*" *> pure Mul)
+    , InfixL (symbol "/" *> pure Div) ]
+  , [ InfixL (symbol "+" *> pure Add)
+    , InfixL (symbol "-" *> pure Sub)
+    , InfixL (symbol "%" *> pure Mod) ]
+  ]
+
+signedFloat :: Parser Double
+signedFloat = L.signed sc (L.lexeme sc L.float)
+
+term :: Parser (Expr Double)
+term = parens expr <|> Number <$> signedFloat
+
+expr :: Parser (Expr Double)
+expr = makeExprParser term operators
+
+-- TODO: Use runParser(T)'
+parse :: String -> Either ParseError (Expr Double)
+parse = runParser expr ""
+
+
+evaluate :: Num a => Expr a -> a
+evaluate = undefined
+
 
 -- | Increment one 'Num' value.
 --
