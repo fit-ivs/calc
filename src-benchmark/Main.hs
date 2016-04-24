388f53c66955e058b6567569e9d839855af7c81c
Jakub Zárybnický <Jakub Zrybnick>
2016-04-24 16:16:57 +0200

    Add current benchmarks

--- src-benchmark/Main.hs
+++ src-benchmark/Main.hs
@@ -1,7 +1,10 @@
 import Criterion
 import Criterion.Main
 
-import Lib (inc)
+import Lib (evaluate, parse, Expr(..))
 
 main :: IO ()
-main = defaultMain [bench "inc 41" (whnf inc (41 :: Int))]
+main = defaultMain
+    [ bench "evaluate (Logarithm (Number 10) (Number 1000))"
+        (whnf evaluate (Logarithm (Number (10 :: Double)) (Number 1000)))
+    , bench "parse 'log 1000'" (whnf parse "log (999 + 1)") ]
