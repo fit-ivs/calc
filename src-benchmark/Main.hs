d23e11e30d25a863630d36e1a6c8e8c3eb211c27
Jakub Zárybnický <kuba.zarybnicky@post.cz>
2016-04-24 22:32:35 +0200

    Fix benchmarks

--- src-benchmark/Main.hs
+++ src-benchmark/Main.hs
@@ -2,9 +2,10 @@ import Criterion
 import Criterion.Main
 
 import Lib (evaluate, parse, Expr(..))
+import System.IO
 
 main :: IO ()
-main = defaultMain
+main = hSetEncoding stdout utf8 >> defaultMain
     [ bench "evaluate (Logarithm (Number 10) (Number 1000))"
         (whnf evaluate (Logarithm (Number (10 :: Double)) (Number 1000)))
     , bench "parse 'log 1000'" (whnf parse "log (999 + 1)") ]
